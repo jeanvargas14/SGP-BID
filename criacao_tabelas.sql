96f3eb8a64a888751ce5e9b18e7d99bf8bd1edce
Bob <roberto.conceicao@softplan.com.br>
2016-06-30 13:48:45 -0300

    atualizado

--- criacao_tabelas.sql
+++ criacao_tabelas.sql
@@ -30,24 +30,20 @@ ALTER TABLE sgpUsuario ADD CONSTRAINT  XPKsgpUsuario  PRIMARY KEY
   cdUsuario
 );
 
-CREATE TABLE sgpGrupo (
-	cdGrupo varchar (255) NOT NULL
-);
-ALTER TABLE sgpGrupo ADD CONSTRAINT  XPKsgpGrupo  PRIMARY KEY 
+CREATE TABLE sgpgrupo
 (
-  cdGrupo
+  cdgrupo character varying(255) NOT NULL,
+  CONSTRAINT xpksgpgrupo PRIMARY KEY (cdgrupo)
 );
 
-CREATE TABLE sgpUsuarioGrupo (
- cdUsuario varchar (255) NOT NULL,
- cdGrupo varchar (255) NOT NULL
-);
-
-ALTER TABLE sgpUsuarioGrupo ADD CONSTRAINT  XPKsgpUsuarioGrupo  PRIMARY KEY 
+CREATE TABLE sgpusuariogrupo
 (
-  cdUsuario,
-  cdGrupo
-); 
+  cdusuario character varying(255) NOT NULL,
+  cdgrupo character varying(255) NOT NULL,
+  CONSTRAINT xpksgpusuariogrupo PRIMARY KEY (cdusuario, cdgrupo),
+  FOREIGN KEY (cdusuario) REFERENCES sgpUsuario(cdusuario),
+  FOREIGN KEY (cdgrupo) REFERENCES sgpGrupo(cdgrupo)
+);
 
 -- Inserindo usuarios para testes os proximos podem ser inseridos via interface
 INSERT INTO sgpUsuario (cdUsuario, senha ) VALUES ( 'sgp' , 'C5nCRry4A+tK/BB0Gc15uQ=='); --senha sgp
