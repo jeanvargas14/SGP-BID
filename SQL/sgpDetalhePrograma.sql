05b417b47a97cafe4c45653d5f5cba88fcf88362
jeanvargas14 <jeanvargas14@gmail.com>
2016-07-19 22:15:38 -0300

    Add files via upload

---
+++ SQL/sgpDetalhePrograma.sql
@@ -0,0 +1,30 @@
+﻿CREATE TABLE sgpDetalhePrograma(
+	cdPrograma decimal (2,0) NOT NULL REFERENCES sgpPrograma (cdPrograma),
+	dtAprovacao date NULL,
+  	dtAssinatura date NULL,
+	dtElegibilidade date NULL,
+	dtConclusao date NULL,
+	dtPrazoUltimoDesembOriginal date NULL,
+	dtPrazoUltimoDesembVigente date NULL,
+	dtPrimeiraAmortizacao date NULL,
+	dtUltimaAmortizacao date NULL,
+	dtApresentacaoPCR date NULL,
+	dtApresentacaoLRR date NULL,
+	dtAnaliseRiscoInicial date NULL,
+	dtRevisaoAnaliseRisco date NULL,
+	dtUltimaVisitaSupervisao date NULL,
+	sgTipoOperacao varchar (50) NULL,
+	tpfonte char (1) NULL,
+	deAtoCriacaoUnidade varchar (200) NULL,
+	deDesignacaoEquipe varchar (200) NULL,
+	sgDivisao varchar (30) NULL,
+	cdEspSetorial decimal (3,0) NULL,
+	cdEspAlternativo decimal (3,0) NULL,
+	cdEspFinanceiro decimal (3,0) NULL,
+	vlOriginalAprovado decimal (12,2) NULL,
+	vlVigente decimal (12,2) NULL,
+  CONSTRAINT XPKsgpDetalhePrograma PRIMARY KEY 
+
+  (cdPrograma)
+
+  );
---
+++ SQL/sgpPrograma.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE sgpPrograma(
+  cdPrograma   decimal (2,0) NOT NULL,
+  nmProjeto   varchar (150) NOT NULL,
+  nmProjetoIngles varchar (150) NOT NULL,
+  nuProjeto varchar(15) NOT NULL,
+  nuOperacao varchar(15) NOT NULL,
+  deProjeto varchar(2000) NOT NULL,
+  nmMutuario varchar (100) NOT NULL,
+  sgMutuario varchar (15) NOT NULL,
+  nuAnoInicio decimal(4,0) NOT NULL,
+  nuAnoFim decimal(4,0) NOT NULL,
+  CONSTRAINT XPKsgpPrograma PRIMARY KEY (cdPrograma));
