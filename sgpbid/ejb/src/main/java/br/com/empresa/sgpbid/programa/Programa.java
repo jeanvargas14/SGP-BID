2c6130886168c609a6ae16e169f5587b82f34f01
Bob <roberto.conceicao@softplan.com.br>
2016-07-25 18:11:50 -0300

    alterado deixando como estava antes.

--- sgpbid/ejb/src/main/java/br/com/empresa/sgpbid/programa/Programa.java
+++ sgpbid/ejb/src/main/java/br/com/empresa/sgpbid/programa/Programa.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
@@ -23,6 +24,7 @@ public class Programa {
 
 	@NotNull
 	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Id
 	private Integer cdPrograma;
 	
 	@NotNull
