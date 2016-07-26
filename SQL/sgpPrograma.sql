5bbb267fc71aa0be93e5ba18ba2f8575644ffca0
jeanvargas14 <jeanvargas14@gmail.com>
2016-07-26 12:22:47 -0300

    Update sgpPrograma.sql

--- SQL/sgpPrograma.sql
+++ SQL/sgpPrograma.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE sgpPrograma(
   cdPrograma   decimal (2,0) NOT NULL,
   nmPrograma   varchar (150) NOT NULL,
-  nmProgramaIngles varchar (150) NOT NULL,
+  nmProgramaIngles varchar (150) NULL,
   nuPrograma varchar(15) NOT NULL,
   nuOperacao varchar(15) NOT NULL,
   dePrograma varchar(2000) NOT NULL,
